5225a4f769813f76d202b9763e71842e6a71a29b
Kara <kara.a.flaherty@gmail.com>
2017-07-11 01:19:21 -0600

    Created one unique post and comment, then connected them in the seed test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,12 +10,24 @@ end
 posts = Post.all
 
 # Create Comments
- 100.times do
+100.times do
   Comment.create!(
     post: posts.sample,
     body: RandomData.random_paragraph
   )
 end
+
+# Create a unique post
+Post.find_or_create_by(
+  title: "Unique post",
+  body: "Unique post body"
+)
+
+# Create a unique comment
+Comment.find_or_create_by(
+  post: Post.find_by_title("Unique post"),
+  body: "Unique comment"
+)
 
 puts "Seed finished"
 puts "#{Post.count} posts created"

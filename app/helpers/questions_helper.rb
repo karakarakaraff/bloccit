d727aca203beaf5120556bf860b10f4119802d3f
Kara <kara.a.flaherty@gmail.com>
2017-07-18 19:57:54 -0600

    Moved RESOLVED logic on index/show views to QuestionsHelper module

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,17 @@
 module QuestionsHelper
+  def index_resolved(question)
+    if question.resolved
+      link_to "RESOLVED: #{question.title}", question
+    else
+      link_to question.title, question
+    end
+  end
+
+  def show_resolved(question)
+    if question.resolved
+      "RESOLVED: #{question.title}"
+    else
+      question.title
+    end
+  end
 end

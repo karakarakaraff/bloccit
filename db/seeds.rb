35278f30010df3ff1334f3be8fc1b75e8e0628e7
Kara <kara.a.flaherty@gmail.com>
2017-07-21 01:03:58 -0600

    Created sponsored posts and nested them wihin topics

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,7 +27,19 @@ posts = Post.all
   )
 end
 
+# Create Sponsored Posts
+50.times do
+  SponsoredPost.create!(
+    topic: topics.sample,
+    title:  RandomData.random_sentence,
+    body:   RandomData.random_paragraph,
+    price:  rand(1..100)
+  )
+end
+sponsored_posts = SponsoredPost.all
+
 puts "Seed finished"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{SponsoredPost.count} sponsored posts created"

7fccab813da7f67382d79335c9046e10a6b71071
Kara <kara.a.flaherty@gmail.com>
2017-07-22 15:10:06 -0600

    Added spec to test user name formatting

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,6 +21,12 @@ RSpec.describe User, type: :model do
     it "should have name and email attributes" do
       expect(user).to have_attributes(name: "Bloccit User", email: "user@bloccit.com")
     end
+
+    it "should format the user name" do
+      user.name = "user name"
+      user.save
+      expect(user.name).to eq "User Name"
+    end
   end
 
   describe "invalid user" do

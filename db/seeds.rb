435469dd66be7470474597c7896be1023ad56f53
Kara <kara.a.flaherty@gmail.com>
2017-07-25 22:20:58 -0600

    Added user roles for member and admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,10 +39,18 @@ posts = Post.all
   )
 end
 
-# Create default users
-user = User.first
-user.update_attributes!(
-  email: 'hello@world.com',
+# Create an admin user
+admin = User.create!(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+)
+
+# Create a member
+member = User.create!(
+  name:     'Member User',
+  email:    'member@example.com',
   password: 'helloworld'
 )
 

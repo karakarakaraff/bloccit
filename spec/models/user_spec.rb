3f6f7b2fad511e30f206b023d58a89bfbc16fb58
Kara <kara.a.flaherty@gmail.com>
2017-07-27 13:34:23 -0600

    First attempt at adding voting -- not yet complete

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,18 +5,16 @@ RSpec.describe User, type: :model do
 
   it { is_expected.to have_many(:posts) }
   it { is_expected.to have_many(:comments) }
+  it { is_expected.to have_many(:votes) }
 
-  # Shoulda tests for name
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_length_of(:name).is_at_least(1) }
 
-  # Shoulda tests for email
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_uniqueness_of(:email) }
   it { is_expected.to validate_length_of(:email).is_at_least(3) }
   it { is_expected.to allow_value("user@bloccit.com").for(:email) }
 
-  # Shoulda tests for password
   it { is_expected.to validate_presence_of(:password) }
   it { is_expected.to have_secure_password }
   it { is_expected.to validate_length_of(:password).is_at_least(6) }

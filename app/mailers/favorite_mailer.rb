a255d926b16e8d6e53a20e2f42cdb7221ddb478f
Kara <kara.a.flaherty@gmail.com>
2017-07-29 15:14:25 -0600

    Added auto-favoriting so a user will receive notifications of new comments on their own posts

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -11,5 +11,15 @@ class FavoriteMailer < ApplicationMailer
     @comment = comment
 
     mail(to: user.email, bcc: "kara.a.flaherty@gmail.com", subject: "New comment on #{post.title}")
-   end
+  end
+
+  def new_post(post)
+    headers["Message-ID"] = "<posts/#{post.id}@your-app-name.example>"
+    headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
+    headers["References"] = "<post/#{post.id}@your-app-name.example>"
+
+    @post = post
+
+    mail(to: post.user.email, subject: "You're following #{post.title}")
+  end
 end

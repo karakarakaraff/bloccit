3ea118404a4e21222e22733317a013206532725a
Kara <kara.a.flaherty@gmail.com>
2017-07-31 00:16:19 -0600

    Added user profiles, refactored tests using factories

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -2,14 +2,14 @@ class FavoriteMailer < ApplicationMailer
   default from: "kara.a.flaherty@gmail.com"
 
   def new_comment(user, post, comment)
-    headers["Message-ID"] = "<comments/#{comment.id}@bloccit-karakarakaraff.herokuapp.com>"
-    headers["In-Reply-To"] = "<post/#{post.id}@bloccit-karakarakaraff.herokuapp.com>"
-    headers["References"] = "<post/#{post.id}@bloccit-karakarakaraff.herokuapp.com>"
+    headers["Message-ID"] = "<comments/#{comment.id}@your-app-name.example>"
+    headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
+    headers["References"] = "<post/#{post.id}@your-app-name.example>"
 
     @user = user
     @post = post
     @comment = comment
 
     mail(to: user.email, bcc: "kara.a.flaherty@gmail.com", subject: "New comment on #{post.title}")
-   end
+  end
 end
